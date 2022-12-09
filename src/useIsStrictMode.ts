c5b39cbd68e8e8417011bb8101003eb94e27024f
Lingfan Gao <lingfangao@hotmail.com>
2022-12-09 19:15:11 +0100

    feat: useDisposable implementation
    Implements the `useDisposable` hook along with tests

--- src/useIsStrictMode.ts
+++ src/useIsStrictMode.ts
@@ -1,5 +1,8 @@
 import * as React from "react";
 
+/**
+ * @returns Current react fiber being rendered
+ */
 export const getCurrentOwner = () =>
   // @ts-ignore - using react internals
   React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner
